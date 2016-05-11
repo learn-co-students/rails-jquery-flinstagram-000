518ce3104fc5210cd67cb7383608fd043b779d5b
NoneOfMaster <rcraigt@gmail.com>
2016-05-11 12:25:56 -0700

    lab complete, although spec passes right off the clone

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,23 @@
-// Add your JavaScript here
+$(function() {
+  addListeners();
+});
+    
+function addListeners(){
+  $(".like").click(function(event){
+    makeAJAXRequest.call(this);
+    return false;
+  });
+};
+
+function makeAJAXRequest(){
+  var obj = $(this);
+  var url = $(this).children("a").attr("href");
+  $.post( url, success )
+};
+
+function success(data){
+  var $thisButton = $(".like-btn[picture_id =" + data["pic_num"] + "]");
+  var $thisMessage = $thisButton.parents(".like").children(".likes-message");
+  $thisButton.children("span").toggleClass("red-heart").toggleClass("grey-heart");
+  $thisMessage.html(data.likes_message)
+};
